9fb0bdd52ae158b31280f53f168415b3b3231df4
Diwakar Rai <111923854+DkGoat66@users.noreply.github.com>
2024-03-09 22:31:30 -0500

    Update food.py

--- food.py
+++ food.py
@@ -1,19 +1,25 @@
 from turtle import Turtle
 import random
 
-
 class Food(Turtle):
 
     def __init__(self):
         super().__init__()
+
+        # Set up the food appearance and position
         self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
+
+        # Initialize the food's position by calling the refresh method
         self.refresh()
 
     def refresh(self):
+        # Generate random coordinates within the screen boundaries
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
+
+        # Move the food to the randomly generated position
         self.goto(random_x, random_y)
