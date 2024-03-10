5baba3e75674d7567a546c725c04d1038a72e28a
Diwakar Rai <111923854+DkGoat66@users.noreply.github.com>
2024-03-09 22:30:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,57 +4,54 @@ from food import Food
 from scoreboard import Scoreboard
 import time
 
+# Set up the screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("My snake Game")
+screen.title("My Snake Game")
 screen.tracer(0)
 
+# Create instances of Snake, Food, and Scoreboard
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
+# Set up keyboard controls
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
+# Game loop
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
 
-    #Detect collision with food
+    # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
-    #DETECT COLLISION WITH WALL
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    # Detect collision with wall
+    if (
+        snake.head.xcor() > 280
+        or snake.head.xcor() < -280
+        or snake.head.ycor() > 280
+        or snake.head.ycor() < -280
+    ):
         scoreboard.reset()
         snake.reset()
 
-    ##DETECT COLLISION WITH TAIL
+    # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             scoreboard.reset()
             snake.reset()
     # IF HEAD COLLIDES WITH ANY SEGMENT IN THE TAIL:
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Exit the game on click
 screen.exitonclick()
