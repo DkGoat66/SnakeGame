fd26938a7b206e523c30de34a78fab0738f1179c
Diwakar Rai <111923854+DkGoat66@users.noreply.github.com>
2024-03-12 23:37:58 -0400

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+
+# Global constants for scoreboard display
 ALIGNMENT = "center"
 FONT = ("Ariel", 24, "normal")
 
@@ -6,8 +8,10 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        # Load high score from an external file
         with open("data.txt") as data:
             self.high_score = int(data.read())
+        # Setting up initial scoreboard appearance
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -16,18 +20,21 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        # Clear existing scoreboard and update with new scores
         self.clear()
-        self.write(f"score: {self.score} High Score: {self.high_score} ", align="center", font=("Ariel", 24, "normal"))
+        self.write(f"score: {self.score} High Score: {self.high_score} ", align=ALIGNMENT, font=FONT)
 
     def reset(self):
-        if self.score> self.high_score:
+        # Update high score if current score surpasses it, reset the score to 0
+        if self.score > self.high_score:
             self.high_score = self.score
+            # Write a new high score to external file
             with open("data.txt", mode="w") as data:
                 data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
-
     def increase_score(self):
+        # Increase score by 1 and update scoreboard
         self.score += 1
         self.update_scoreboard()
